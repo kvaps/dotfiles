83c26b7f2ebbe241ab3c21b4038799ee94f921d9
kvaps <kvapss@gmail.com>
2019-09-21 23:03:35 +0200

    upd: awesome config

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -181,7 +181,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "www", " ⟙2 ", " ⟙3 ", " ⟙4 ", "keepass", "email", " ★7 ", " ★8 ", " ♫ " }, s, awful.layout.layouts[1])
+    awful.tag({ " www  ", " ⟙2 ", " ⟙3 ", " ⟙4  ", " ⩷⩷⩷ ", " ★6 ", " ★7 ", " ★8 ", " ♫ " }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
