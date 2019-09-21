eaabeebbb2e7fb849e7bd1a3c077ba549bff1770
kvaps <kvapss@gmail.com>
2019-09-22 01:13:41 +0200

    upd: awesome theme

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -17,9 +17,9 @@ theme.bg_normal     = "#222222"
 theme.bg_focus      = "#664100"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
-theme.bg_systray    = theme.bg_normal
+theme.bg_systray    = theme.fg_normal
 
-theme.fg_normal     = "#aaaaaa"
+theme.fg_normal     = "#ffb433"
 theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
