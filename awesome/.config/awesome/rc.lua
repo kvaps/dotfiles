14eef8330964b94cbb60cf62c06cc6b538456f99
kvaps <kvapss@gmail.com>
2020-02-21 18:25:12 +0100

    add: termite configuration

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -42,7 +42,7 @@ end
 beautiful.init("~/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "termite"
 browser = "firefox"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
